b0ba1652fcb2deaea258bdd28c07c82be3db0518
R3call <thenoobyzombie@gmail.com>
2023-10-09 23:15:18 +0200

    Converted project to tsx

--- pages/api/hello.js
+++
@@ -1,7 +0,0 @@
-// req is an instance of http.IncomingMessage
-// res is an instance of http.ServerResponse
-function handler(req, res) {
-    res.status(200).json({ text: 'Hello' });
-}
-
-export default handler;
