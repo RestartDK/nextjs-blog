77d23b38dd73b16f8c34032f514191624315bd4d
R3call <thenoobyzombie@gmail.com>
2023-10-10 10:55:56 +0200

    fixing hello again

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,7 +1,7 @@
 // req is an instance of http.IncomingMessage
 // res is an instance of http.ServerResponse
 function handler(req, res) {
-    res.status(200).json({ text: 'Hello Again' });
+    res.status(200).json({ text: 'Hello' });
 }
 
 export default handler;
