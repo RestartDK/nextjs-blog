c9a9dd1872803a39019eaf127fcb836d9a3c8007
R3call <thenoobyzombie@gmail.com>
2023-10-10 10:43:59 +0200

    Changed hello again

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,5 +1,7 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+// req is an instance of http.IncomingMessage
+// res is an instance of http.ServerResponse
+function handler(req, res) {
+    res.status(200).json({ text: 'Hello' });
+}
 
-export default (_: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ text: 'Hello' });
-};
+export default handler;
